0fc0e2405bf4bbbeccaf99c1c90322ea7c272941
MJoshua25 <jyao2000@yahoo.fr>
2019-10-10 13:39:58 +0000

    apiviews

---
+++ event/appevent/apiviews.py
@@ -0,0 +1,27 @@
+
+from rest_framework import viewsets,filters
+
+from .models import .
+from .serializers import ParticipantsSerializer, EventsSerializer, CommuneSerializer, Categorie_eventSerializer, CompagnieSerializer
+
+# class DynamicSearchFilter(filters.SearchFilter):
+#     def get_search_fields(self, view, request):
+#         return request.GET.getlist('search_fields', [])
+
+
+class CompagnieViewset(viewsets.ModelViewSet):
+    filter_backends = (DynamicSearchFilter,)
+    queryset = Poll.objects.all()
+    serializer_class = CompagnieSerializer
+
+class Categorie_eventViewset(viewsets.ModelViewSet):
+    queryset = Choice.objects.all()
+    serializer_class = Categorie_eventSerializer
+
+class CommuneViewset(viewsets.ModelViewSet):
+    queryset = Vote.objects.all()
+    serializer_class = CommuneSerializer
+
+class EventsViewset(viewsets.ModelViewSet):
+    queryset = Vote.objects.all()
+    serializer_class = EventsSerializer
--- event/appevent/serializers.py
+++ event/appevent/serializers.py
@@ -3,20 +3,18 @@ from drf_dynamic_fields import DynamicFieldsMixin
 
 from .models import *
 
+class ParticipantsSerializer(DynamicFieldsMixin, serializers.ModelSerializer):
+    class Meta:
+        model = Participants
+        fields = '__all__'
 
 
 class EventsSerializer(DynamicFieldsMixin, serializers.ModelSerializer):
+    participant_event = ParticipantsSerializer(many=True, required=False)
     class Meta:
         model = Events
         fields = '__all__'
 
-
-class ParticipantsSerializer(DynamicFieldsMixin, serializers.ModelSerializer):
-    id_event = EventsSerializer(many=True, required=False)
-
-    class Meta:
-        model = Users
-        fields = '__all__'
 
 class CommuneSerializer(DynamicFieldsMixin, serializers.ModelSerializer):
     event_commune = EventsSerializer(many=True, required=False)
@@ -25,14 +23,20 @@ class CommuneSerializer(DynamicFieldsMixin, serializers.ModelSerializer):
         model = Commune
         fields = '__all__'
 
-        
+
 
 class Categorie_eventSerializer(DynamicFieldsMixin, serializers.ModelSerializer):
-    choices = ChoiceSerializer(many=True, read_only=True, required=False)
+    event_categorie = EventsSerializer(many=True, read_only=True, required=False)
 
     class Meta:
-        model = Poll
+        model = Categorie_event
         fields = '__all__'
 
 
 
+class CompagnieSerializer(DynamicFieldsMixin, serializers.ModelSerializer):
+    event_compagnie = EventsSerializer(many=True, read_only=True, required=False)
+
+    class Meta:
+        model = Compagnie
+        fields = '__all__'
