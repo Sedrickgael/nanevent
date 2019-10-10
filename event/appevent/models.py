129949e766dfb770b0899b4f8b0bb0a5e47f41ff
MJoshua25 <jyao2000@yahoo.fr>
2019-10-10 13:12:45 +0000

    se

--- event/appevent/models.py
+++ event/appevent/models.py
@@ -1,16 +1,10 @@
-from django.db import models
-
 class Compagnie(models.Model):
 
     nom_compagnie = models.CharField(max_length=255)
-    addresse = models.CharField(max_length=255)
+    addresse = models.CharField(max_length=50)
     email = models.CharField(max_length=255)
     contact = models.CharField(max_length=255)
     password = models.CharField(max_length=255)
-    date_udapte = models.DateField(auto_now=False,)
-    dat_add = models.DateField(auto_now=False, auto_now_add=False)
-    statut = models.BooleanField()
-    
     
     def __str__(self):
         return self.nom_compagnie
@@ -22,10 +16,8 @@ class Compagnie(models.Model):
         verbose_name_plural = 'Compagnies'
         
 class Commune(models.Model):
+    
     nom = models.CharField(max_length=255)
-    date_udapte = models.DateField(auto_now=False,)
-    dat_add = models.DateField(auto_now=False, auto_now_add=False)
-    statut = models.BooleanField()
     
     def __str__(self):
         return self.nom
@@ -38,18 +30,14 @@ class Commune(models.Model):
 
 class Events(models.Model):
 
-    nom_event = models.CharField(max_length=255)
+    nom_event = models.CharField(max_length=50)
     date_debut = models.DateField(, auto_now=False, auto_now_add=False)
     date_fin = models.DateField(, auto_now=False, auto_now_add=False)
-    id_commune = models.ForeignKey('Commune', on_delete=models.CASCADE, related_name='event_commune')
-    compagnie_id = models.ForeignKey('Compagnie', on_delete=models.CASCADE, related_name='event_compagnie')
-    id_categorie = models.ForeignKey('Categorie_event', on_delete=models.CASCADE, related_name='event_categorie')
+    id_commune = models.ForeignKey('Commune', on_delete=models.CASCADE)
+    compagnie_id = models.ForeignKey('Compagnie', on_delete=models.CASCADE)
+    id_categorie = models.ForeignKey('Categorie_event', on_delete=models.CASCADE)
     statut = models.BooleanField(default=False)
-    description  = models.CharField(max_length=255)
-    lieu = models.CharField(max_length=255)
-    date_udapte = models.DateField(auto_now=False,)
-    dat_add = models.DateField(auto_now=False, auto_now_add=False)
-    statut = models.BooleanField()
+    description  = models.CharField(max_length=50)
 
     def __str__(self):
         return self.nom_event
@@ -60,8 +48,9 @@ class Events(models.Model):
         verbose_name = 'Events'
         verbose_name_plural = 'Eventss'
 
-class Users(models.Model):
 
+class Users(models.Model):
+    
     id_commune = models.ForeignKey('Commune', on_delete=models.CASCADE)
     nom = models.CharField(max_length=255)
     prenom = models.CharField(max_length=255)
@@ -113,3 +102,4 @@ class Participants(models.Model):
         managed = True
         verbose_name = 'Participants'
         verbose_name_plural = 'Participantss'
+        verbose_name_plural = 'Eventss'
--- event/appevent/serializers.py
+++ event/appevent/serializers.py
@@ -4,32 +4,35 @@ from drf_dynamic_fields import DynamicFieldsMixin
 from .models import *
 
 
+
 class EventsSerializer(DynamicFieldsMixin, serializers.ModelSerializer):
     class Meta:
         model = Events
         fields = '__all__'
 
 
+class ParticipantsSerializer(DynamicFieldsMixin, serializers.ModelSerializer):
+    id_event = EventsSerializer(many=True, required=False)
+
+    class Meta:
+        model = Users
+        fields = '__all__'
+
 class CommuneSerializer(DynamicFieldsMixin, serializers.ModelSerializer):
-    votes = EventsSerializer(many=True, required=False)
+    event_commune = EventsSerializer(many=True, required=False)
 
     class Meta:
         model = Commune
         fields = '__all__'
 
-
-class CompagnieSerializer(DynamicFieldsMixin, serializers.ModelSerializer):
-    votes = EventsSerializer(many=True, required=False)
-
-    class Meta:
-        model = Compagnie
-        fields = '__all__'
-
         
 
-class PollSerializer(DynamicFieldsMixin, serializers.ModelSerializer):
+class Categorie_eventSerializer(DynamicFieldsMixin, serializers.ModelSerializer):
     choices = ChoiceSerializer(many=True, read_only=True, required=False)
 
     class Meta:
         model = Poll
         fields = '__all__'
+
+
+
